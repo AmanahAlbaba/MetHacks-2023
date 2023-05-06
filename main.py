e8c10fcb0738c2acd135571beab944c0bbaa23cc
AdamSzava <adam.szava@ryerson.ca>
2023-05-05 20:51:56 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# hello
+import PySimpleGUI as sg
+
+# see all the themes
+#sg.theme_previewer()
+
+projectName = 'MetHacks 2023'
+
+sg.theme('LightBlue')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window(f'{projectName}', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+
+window.close()

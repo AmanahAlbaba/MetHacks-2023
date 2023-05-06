008f4d9ab9fcb1d8fe08b1d9f502e49b9624dff7
AdamSzava <adam.szava@ryerson.ca>
2023-05-05 21:31:14 -0400

    small edits

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ sg.theme('LightBlue')   # Add a touch of color
 # All the stuff inside your window.
 # layout contains all the visible elements on the screen
 # each list is its own row
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok')] ]
+layout = [  [sg.Text('Some text on Row 1', key = '-TEXT1-')],
+            [sg.Text('Enter something on Row 2'), sg.InputText(key = '-INPUT-')],
+            [sg.Button('Ok', key = '-BUTTON1-')] ]
 
 # Create the Window
 window = sg.Window(f'{projectName}', layout)
@@ -20,5 +20,13 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
         break
+    print(event)
+    print(values)
 
+    if event == '-BUTTON1-':
+        print('Hello!')
+        # update the text
+        window['-TEXT1-'].update('WOWOWOWOWOOWOW')
+
+    
 window.close()
